3ace09124ed8e65cab198c09e06dff3d36976473
Arbiter09 <jasdhiren.shah@stonybrook.edu>
2025-03-06 14:24:51 -0500

    Initialized fuzz testing

---
+++ test/fuzz/Invaritants.t.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+// Have our invartiant aka properties that should always hold true all the time
+// What are our invariants?
+// 1. The total supply of DSC should be less than the total value of collateral
+// 2. Getter view functions should never revert <- evergreen invariant
+
+import {Test, console} from "forge-std/Test.sol";
+import {StdInvariant} from "forge-std/StdInvariant.sol";
+import {DeployDSC} from "../../script/DeployDSC.s.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {DSCEngine} from "../../src/DSCEngine.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract Invariants is StdInvariant, Test {
+    DeployDSC deployer;
+    DecentralizedStableCoin dsc;
+    DSCEngine engine;
+    HelperConfig config;
+    address weth;
+    address wbtc;
+
+    function setUp() external {
+        deployer = new DeployDSC();
+        (dsc, engine, config) = deployer.run();
+        (,, weth, wbtc,) = config.activeNetworkConfig();
+
+        console.log("DSCEngine Address: ", address(engine));
+        console.log("DSC Address: ", address(dsc));
+        console.log("WETH Address: ", weth);
+        console.log("WBTC Address: ", wbtc);
+
+        require(address(engine) != address(0), "DSCEngine address is zero!");
+
+        targetContract(address(engine));
+    }
+
+    function invariant_protocolMustHaveMoreValueThanTotalSupply() public view {
+        // get the value of all the collateral in the protocol
+        // compare it to all the debt (dsc)
+
+        uint256 totalSupply = dsc.totalSupply();
+        uint256 totalWethDeposited = IERC20(weth).balanceOf(address(engine));
+        uint256 totalWbtcDeposited = IERC20(wbtc).balanceOf(address(engine));
+
+        uint256 wethValue = engine.getUsdValue(weth, totalWethDeposited);
+        uint256 wbtcValue = engine.getUsdValue(wbtc, totalWbtcDeposited);
+
+        console.log(wethValue);
+        console.log(wbtcValue);
+
+        assert(wethValue + wbtcValue >= totalSupply);
+    }
+}
